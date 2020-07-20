8e765ae91959e9e99873eb13b447555ff7c64829
ccwq <scwqd@qq.com>
2020-07-20 09:18:29 +0800

    v-box滚动样式修正

--- src/comp/v-box/v-box.vue
+++ src/comp/v-box/v-box.vue
@@ -99,8 +99,8 @@
                 position: absolute;
                 left: 0;
                 top: 0;
-                right: 0;
-                bottom: 0;
+                height: 100%;
+                width: 100%;
                 overflow-y: auto;
             }
         }
