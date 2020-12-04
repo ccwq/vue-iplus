06d8c450eaf70fa5021259b1a0383a6448deb942
ccwq <scwqd@qq.com>
2020-12-04 23:55:53 +0800

    echarts api扩展

--- src/comp/e-charts/index.js
+++ src/comp/e-charts/index.js
@@ -1,6 +1,8 @@
 import darkThemeRunner from "./dark.theme"
 import echartsComp from "./ECharts.vue";
 
+const noop = _ => _;
+
 export const colorLs = [
     "#feb64d",
     "#ff7c7c",
@@ -17,8 +19,34 @@ export const getColor = function(index){
 }
 
 export default {
-    install(Vue, options){
-        const {echarts, name="e-charts", defaultTheme="dark"} = options;
+
+    /**
+     * 安装echarts
+     * @param Vue
+     * @param options 可以传入一个配置对象或者直接传入echarts实例
+     * @param options 可以为echarts实例
+     * @param options.echarts
+     * @param options.name 绑定组件的名称
+     * @param options.defaultTheme 默认主题
+     * @param options.baseOption 默认配置
+     * @param options.optionHook 配置被应用前允许在此进行修改
+     */
+    install(Vue, options = {}){
+
+        //允许直接传入echarts实例
+        if (options.version) {
+            options = {
+                echarts:options
+            }
+        }
+
+        const {
+            echarts,
+            name = "e-charts",
+            defaultTheme = "dark",
+            baseOption = {},
+            optionHook = noop,
+        } = options;
 
         if (!echarts) {
             throw new Error("require field lost:options.echarts");
@@ -26,11 +54,31 @@ export default {
 
         darkThemeRunner(echarts);
 
-        //注入echarts
-        echartsComp.props.__apache_echarts = {
-            default: echarts,
-        };
-        echartsComp.props.theme.default = defaultTheme;
+        const props = echartsComp.props;
+
+
+
+        Object.assign(props, {
+
+            //注入echarts
+            __apache_echarts: {
+                default: __ => echarts,
+            },
+
+            //主题
+            theme:{
+                default: defaultTheme
+            },
+
+            baseOption:{
+                default: _ => baseOption
+            },
+
+            optionHook:{
+                default:_=>optionHook,
+            },
+        });
+
         echartsComp.name = name;
         Vue.component(name, echartsComp);
     }
