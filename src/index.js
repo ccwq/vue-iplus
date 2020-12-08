ebb819285583822d0f9d042df2b8f442657f7ee1
ccwq <scwqd@qq.com>
2020-12-08 23:01:10 +0800

    hookPromise加入全局install

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import debounce from "./mixin/debounce-method";
 import throttle from "./mixin/throttle-method";
 import debugMix from "./mixin/console-by-name";
+import hookPromiseMix from "./mixin/hook-promise";
 import {mixin as accessVueByDomMixin} from "./mixin/access-vue-by-dom";
 import resizer from "./comp/resizer"
+
 import clickoutside from "./directives/clickoutside";
 
 /**
@@ -19,12 +21,13 @@ export default {
      * @param options.key 在window上绑定的对象名
      */
     install(vue, options) {
-        
+
         //mixin
         vue.mixin(debounce);
         vue.mixin(throttle);
         vue.mixin(accessVueByDomMixin);
         vue.mixin(debugMix(options));
+        vue.mixin(hookPromiseMix);
 
         //组件
         vue.component(resizer.name, resizer);
